4b692b185768ae3e3e3875a640cd8b788ce63b83
Lkacik <cep6@poczta.fm>
2022-06-10 22:34:15 +0200

    clearing counter

--- cancellation.py
+++ cancellation.py
@@ -12,8 +12,8 @@ def cancellation_room_reservation():
 
     with open('room.json', 'r', encoding='utf-8') as read_file:
         room_reservation_dict = json.load(read_file)
+        condition_fulfillment_counter = 0
         for index_number in range(1, len(room_reservation_dict) + 1):
-            condition_fulfillment_counter = 0
             if first_name == room_reservation_dict.get(str(index_number))['imie'] and last_name == room_reservation_dict.get(str(index_number))['nazwisko'] and date_from == room_reservation_dict.get(str(index_number))['data wynajmu'][0]:
                 condition_fulfillment_counter += 1
                 keys_reservation = ['imie', 'nazwisko', 'data wynajmu', 'nr telefonu', 'ilosc lozek', 'uwagi']
@@ -23,5 +23,5 @@ def cancellation_room_reservation():
                 print(room_reservation_dict)
                 with open('room.json', 'w', encoding='utf-8') as save_file:
                     json.dump(room_reservation_dict, save_file, ensure_ascii=False)
-            if condition_fulfillment_counter == 0:
-                print('Błąd, brak rezerwacji o takich danych.')
+        if condition_fulfillment_counter == 0:
+            print('Błąd, brak rezerwacji o takich danych.')
