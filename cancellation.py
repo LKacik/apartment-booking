86beb96755bc46150034ba2a250d266486250b33
Lkacik <cep6@poczta.fm>
2022-06-10 21:32:05 +0200

    added block comment and counter

--- cancellation.py
+++ cancellation.py
@@ -1,6 +1,9 @@
 import json
 
 
+'''function that allows you to delete a reservation, in the key value 'imie', 'nazwisko', 'data wynajmu 'insert None'''
+
+
 def cancellation_room_reservation():
     print('Proszę podać dane anulowanej rezerwacji: ')
     first_name = input('Imię wynajmującego: ')
@@ -10,14 +13,15 @@ def cancellation_room_reservation():
     with open('room.json', 'r', encoding='utf-8') as read_file:
         room_reservation_dict = json.load(read_file)
         for index_number in range(1, len(room_reservation_dict) + 1):
+            condition_fulfillment_counter = 0
             if first_name == room_reservation_dict.get(str(index_number))['imie'] and last_name == room_reservation_dict.get(str(index_number))['nazwisko'] and date_from == room_reservation_dict.get(str(index_number))['data wynajmu'][0]:
-                del room_reservation_dict[str(index_number)]
+                condition_fulfillment_counter += 1
+                keys_reservation = ['imie', 'nazwisko', 'data wynajmu']
+                for key_reservation in keys_reservation:
+                    room_reservation_dict[str(index_number)][key_reservation] = None
                 print('Rezerwacja, anulowana pomyślnie.')
                 print(room_reservation_dict)
                 with open('room.json', 'w', encoding='utf-8') as save_file:
                     json.dump(room_reservation_dict, save_file, ensure_ascii=False)
-                exit()
-        print('Błąd, brak rezerwacji o takich danych.')
-
-
-cancellation_room_reservation()
+            if condition_fulfillment_counter == 0:
+                print('Błąd, brak rezerwacji o takich danych.')
